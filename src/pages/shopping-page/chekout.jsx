9d0a598d161b149ed7aae4d3d7f0dea135e655a8
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2024-09-26 00:33:49 +0530

    Update chekout.jsx

--- src/pages/shopping-page/chekout.jsx
+++ src/pages/shopping-page/chekout.jsx
@@ -29,7 +29,7 @@ function ShoppingCheckout() {
               : currentItem?.price) *
               currentItem?.quantity,
           0
-        ).toFixed(2)
+        )
       : 0;
 
   function handleInitiatePaypalPayment() {
