71c5ab8779dc6f5a3a2a5c270ae0344f7a743883
Ajay Kumar <96405422+AjayKumar-DRB@users.noreply.github.com>
2024-09-25 20:10:04 +0530

    Update image-upload.jsx

--- src/components/admin-view/image-upload.jsx
+++ src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:5000/api/admin/products/upload-image",
+      "https://zapkart-backend.onrender.com/api/admin/products/upload-image",
       data
     );
     console.log(response, "response");
